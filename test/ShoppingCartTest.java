abf8f75889ea12ab5a09ba23b1396da0e2048152
Jurandi <jur.and.jr42@gmail.com>
2022-06-26 17:32:01 -0300

    Criação do teste itemRemovidoDoCarrinhoDecresceOContadorDeItens e refatoração com o método setup()

--- test/ShoppingCartTest.java
+++ test/ShoppingCartTest.java
@@ -1,39 +1,64 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ShoppingCartTest {
+	
+	ShoppingCart cart = null;
+	Product product1 = null;
+	Product product2 = null;
+	Product product3 = null;
+	Product product4 = null;
+	
+	@Before
+	public void setup() {
+		cart = new ShoppingCart();
+		product1 = new Product("Nintendo 3DS", 129.99);
+		product2 = new Product("Nintendo Wii", 149.99);
+		product3 = new Product("Nintendo Switch", 179.99);
+		product4 = new Product("The Legend of Zelda", 60.00);
+	}
 
 	@Test
 	public void carrinhoNovoPossuiZeroItens() {
-		ShoppingCart cart = new ShoppingCart();
+		setup();
 		assertEquals(0, cart.getItemCount());
 	}
 	
 	@Test
 	public void carrinhoVazioPossuiZeroItens() {
-		ShoppingCart cart = new ShoppingCart();
-		cart.addItem(new Product("Nintendo 3DS", 129.99));
-		cart.addItem(new Product("Nintendo 3DSXL", 179.99));
+		setup();
+		cart.addItem(product1);
+		cart.addItem(product2);
 		cart.empty();
 		assertEquals(0, cart.getItemCount());
 	}		
 	
 	@Test
 	public void totalDeDoisItensNoCarrinho() {
-		ShoppingCart cart = new ShoppingCart();
-		cart.addItem(new Product("Nintendo 3DS", 129.99));
-		cart.addItem(new Product("Nintendo Switch", 179.99));
+		setup();
+		cart.addItem(product1);
+		cart.addItem(product3);
 		assertEquals(2, cart.getItemCount());
 	}
 	
 	@Test
 	public void produtoAdicionadoAoCarrinhoAtualizaSaldo() {
-		ShoppingCart cart = new ShoppingCart();
-		cart.addItem(new Product("Nintendo 3DS", 129.99));
-		cart.addItem(new Product("Nintendo Wii", 179.99));
-		cart.addItem(new Product("The Legend of Zelda", 60.00));
+		setup();
+		cart.addItem(product1);
+		cart.addItem(product3);
+		cart.addItem(product4);
 		assertEquals(369.98, cart.getBalance(), 0.000000001d);
+	}
+	
+	@Test
+	public void itemRemovidoDoCarrinhoDecresceOContadorDeItens() throws ProductNotFoundException {
+		setup();
+		cart.addItem(product1);
+		cart.addItem(product3);
+		cart.removeItem(product3);
+		assertEquals(1, cart.getItemCount());
 	}
 
 }
