e668e4e1fe51cbb8b55926c1e397510c4de32b26
Jurandi <jur.and.jr42@gmail.com>
2022-06-27 16:54:54 -0300

    Refatoração dos testes, separando cada caso em um teste diferente

--- test/toIntegerTest.java
+++ test/toIntegerTest.java
@@ -5,19 +5,51 @@ import org.junit.Test;
 public class toIntegerTest {
 
 	@Test
-	public void test() {
-		assertEquals(-3, Parser.toInteger("-3"));
+	public void numeroInteiroPositivo() {
 		assertEquals(500, Parser.toInteger("500"));
-		assertEquals(-10, Parser.toInteger("-10"));
+	}
+	
+	@Test
+	public void numeroInteiroNegativo() {
+		assertEquals(-3, Parser.toInteger("-3"));
+	}
+	
+	@Test
+	public void maiorNumeroInteiroPositivoPermitido() {
 		assertEquals(32767, Parser.toInteger("32767"));
 	}
 	
+	@Test
+	public void menorNumeroInteiroNegativoPermitido() {
+		assertEquals(-32768, Parser.toInteger("-32768"));
+	}
+	
+	
 	@Test(expected=NumberFormatException.class)
-	public void argumentosInvalidos() {
+	public void naoDeveAceitarEspacosEmBranco() {
 		assertEquals(23, Parser.toInteger("2 3"));
+		
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void naoDeveAceitarCaractere() {
 		assertEquals(3, Parser.toInteger("A3"));
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void naoDeveAceitarNumeroDecimal() {
 		assertEquals(2.3, Parser.toInteger("2.3"), 0.000000001d);
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void naoDeveAceitarNumeroAcimaDoLimiteDeShort() {
 		assertEquals(32768, Parser.toInteger("32768"));
 	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void naoDeveAceitarNumeroAbaixoDoLimiteDeShort() {
+		assertEquals(-32769, Parser.toInteger("-32769"));
+	}
+	
 	
 }
